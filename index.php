40e8aede5ee85a6772f0321ee87c950deca5ce11
snowlines <pengbo@pengbos-MacBook-Pro.local>
2016-04-28 18:59:44 +0800

    更新为snowlines主页
    将默认模板更改为snowlines主页

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8">
+	<title>
+	SNOWLINES运动俱乐部
+	</title>
+	<link rel="stylesheet" type="text/css" href="/myweb/semantic/dist/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="/myweb/css/index.css">
+</head>
+<body>
+	<div class="content">
+		<div class="content_first">
+			<div class="intro">
+				<div class="intro_title">欢迎加入！<br>SNOWLINES运动俱乐部</div>
+				<p>这是一个正在进化的最酷、最炫、最逗逼、最有温度的大家庭。无论是滑雪、滑板、潜水或是冲浪，你都能在这里找到伙伴和你分享。欢迎加入我们，和我们一起享受运动带给我们的乐趣！</p>
+				<div class="wx">r
+					<div class="wx_title">加入我们，请关注微信公众号</div>
+					<img class="wx_code" src="/myweb/img/wx_code.jpg">
+				</div>
+			</div>
+			<div class="logo">
+				<img class="logo_logo" src="/myweb/img/logo.jpg">
+			</div>
+			
+		</div>
+	</div>
+</body>
+</html>
+
+
+
